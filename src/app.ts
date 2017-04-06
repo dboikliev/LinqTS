a9421f8c29e4a4317fc659a8c20df6c4dfc58838
dboikliev <deyanpb@hotmail.com>
2017-04-06 09:28:27 +0300

    Fixes tslint warning.

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ let people: IPerson[] = [{ name: "Ivan", age: 24 }, { name: "Deyan", age: 25 }];
 
 let namesOfPeopleOlderThan22 = linq(people)
     .where(p => p.age > 22)
-    .select(p => p.name)
+    .select(p => p.name);
 
 for (let length of namesOfPeopleOlderThan22) {
     console.log(length);
