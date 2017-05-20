0d087c3133c482b927f7cf2fee146a15563244b8
dboikliev <deyanpb@hotmail.com>
2017-05-20 09:24:25 +0300

    Fixes tslint warnings.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-import { 
+import {
     range,
     linq
 } from "./linq";
 
 let first = [{ name: "Pesho" }];
 let second = [{ name: "Pesho" }];
-
 
 range(1, 1, 10).union(range(5, 1, 20)).forEach(e => console.log(e));
