9a7efc418975a532bc9fb2ff0fcbb12c1f7bf1ca
dboikliev <deyanpb@hotmail.com>
2017-10-20 11:04:28 +0300

    Fix.

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,3 @@ console.log(seq(0, 1, 100)
 .thenBy(x => x % 2)
 .thenBy(x => x.toString().length)
 .toArray())
-
-seq(0, 1, 100)
-.select(x => x.toString())
-.sort({
-    comparer: (a, b) => a - b,
-    selector: {
-        asc: true,
-        property: x => x.length
-    }
-})
