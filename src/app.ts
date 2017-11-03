fdb126fef42dbff7d23b66ec4e676766a6fc1547
dboikliev <deyanpb@hotmail.com>
2017-11-03 13:06:46 +0200

    Removes old liqn.ts file.

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,10 @@ import {
     seq
 } from "./linq";
 
-<<<<<<< HEAD
 
 let reversed = seq(-10, 1, 10)
 .orderBy(x => x == 0 ? 1 : Math.sign(x))
 .thenByDescending(Math.abs)
-=======
-let reversed = linq([1, 2, 3, 4, 5, 6, 7, 8])
-    .reverse()
->>>>>>> master
 
 for (let element of reversed) {
     console.log(element);
