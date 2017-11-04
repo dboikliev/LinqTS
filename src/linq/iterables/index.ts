b942c99dc9ea5b4664182d5c68eba3bac28e8278
dboikliev <deyanpb@hotmail.com>
2017-11-04 12:29:29 +0200

    Removes redundant imports.

--- src/linq/iterables/index.ts
+++ src/linq/iterables/index.ts
@@ -1,39 +1,18 @@
-import { Concat } from "./concat";
-import { Distinct } from "./distinct";
-import { Except } from "./except";
-import { Group } from "./group";
-import { Intersect } from "./intersect";
-import { Join } from "./join";
-import { Ordered } from "./ordered";
-import { Reverse } from "./reverse";
-import { Select } from "./select";
-import { SelectMany } from "./selectMany";
-import { Skip } from "./skip";
-import { SkipWhile } from "./skipWhile";
-import { Take } from "./take";
-import { TakeWhile } from "./takeWhile";
-import { Union } from "./union";
-import { Where } from "./where";
-import { Windowed } from "./windowed";
-import { Zip } from "./zip";
-
-export { 
-    Concat, 
-    Distinct,
-    Except, 
-    Group, 
-    Intersect, 
-    Join, 
-    Ordered, 
-    Reverse, 
-    Select, 
-    SelectMany,
-    Skip, 
-    SkipWhile,
-    Take,
-    TakeWhile,
-    Union,
-    Where,
-    Windowed,
-    Zip
-}
+export { Concat } from "./concat";
+export { Distinct } from "./distinct";
+export { Except } from "./except";
+export { Group } from "./group";
+export { Intersect } from "./intersect";
+export { Join } from "./join";
+export { Ordered } from "./ordered";
+export { Reverse } from "./reverse";
+export { Select } from "./select";
+export { SelectMany } from "./selectMany";
+export { Skip } from "./skip";
+export { SkipWhile } from "./skipWhile";
+export { Take } from "./take";
+export { TakeWhile } from "./takeWhile";
+export { Union } from "./union";
+export { Where } from "./where";
+export { Windowed } from "./windowed";
+export { Zip } from "./zip";
