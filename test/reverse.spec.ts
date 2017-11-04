5dc5af3e536528aa7b28bada6630226f869f0a32
dboikliev <deyanpb@hotmail.com>
2017-11-04 17:36:09 +0200

    Unit tests for reverse.

---
+++ test/reverse.spec.ts
@@ -0,0 +1,12 @@
+import * as assert from "assert";
+import { linq } from "../src/linq";
+
+describe("reverse tests", () => {
+    it("should reverse [1, 2, 3, 4, 5] into [5, 4, 3, 2, 1]", () => {
+        const reversed = linq([1, 2, 3, 4, 5])
+            .reverse()
+            .toArray();
+        
+        assert.deepStrictEqual(reversed, [5, 4, 3, 2, 1]);
+    })
+});
