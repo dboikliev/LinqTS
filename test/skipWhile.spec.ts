2c4f977daeae55f7eee19b3463a225c5476dd65c
dboikliev <deyanpb@hotmail.com>
2017-11-04 17:41:56 +0200

    Adds unit tests for skipWhile.

---
+++ test/skipWhile.spec.ts
@@ -0,0 +1,12 @@
+import * as assert from "assert";
+import { linq } from "../src/linq";
+
+describe("skipWhile tests", () => {
+    it("should skip elements of [2, 4, 6, 7, 8, 10] while they are even", () => {
+        const elements = linq([2, 4, 6, 7, 8, 10])
+            .skipWhile(x => x % 2 == 0)
+            .toArray();
+
+        assert.deepStrictEqual(elements, [7, 8, 10]);
+    });
+});
