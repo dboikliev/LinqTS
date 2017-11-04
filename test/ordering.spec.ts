8002fd45761e31ba9c142c3150c701280e581bb4
dboikliev <deyanpb@hotmail.com>
2017-11-04 10:21:04 +0200

    Fixes typos.

--- test/ordering.spec.ts
+++ test/ordering.spec.ts
@@ -2,12 +2,12 @@ import * as assert from "assert";
 import { linq } from "../src/linq";
 
 describe("orderBy tests", () => {
-    it("[5, 4, 3, 2, 1] should be sorted in asending order", () => {
+    it("[5, 4, 3, 2, 1] should be sorted in ascending order", () => {
         const sorted = linq([5, 4, 3, 2, 1])
             .orderBy(x => x)
             .toArray();
         
-        assert.deepEqual(sorted, [1, 2, 3, 4, 5], "elements in asending order")
+        assert.deepEqual(sorted, [1, 2, 3, 4, 5], "elements in ascending order")
     });
 
     it("[1, 2, 3, 4, 5] should remain sorted in ascending order", () => {
@@ -15,7 +15,7 @@ describe("orderBy tests", () => {
             .orderBy(x => x)
             .toArray();
     
-        assert.deepEqual(sorted, [1, 2, 3, 4, 5], "elements in asending order");
+        assert.deepEqual(sorted, [1, 2, 3, 4, 5], "elements in ascending order");
     });
 
     it("['c', 'ab', 'ccc', 'aa', 'd', 'ba'] should be sorted lexicographically", () => {
@@ -23,6 +23,6 @@ describe("orderBy tests", () => {
             .orderBy(x => x)
             .toArray();
     
-        assert.deepEqual(sorted, ['aa', 'ab', 'ba', 'c', 'ccc', 'd'], "elements in asending order");
+        assert.deepEqual(sorted, ['aa', 'ab', 'ba', 'c', 'ccc', 'd'], "elements in ascending order");
     });
 });
