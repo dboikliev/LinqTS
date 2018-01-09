4ebf24c5ef187aa96fa72bae06114889c8bac8e6
dboikliev <deyanpb@hotmail.com>
2018-01-09 23:19:13 +0200

    Test empty linqable toArray;9

--- test/toArray.spec.ts
+++ test/toArray.spec.ts
@@ -2,9 +2,16 @@ import * as assert from "assert"
 import { linq } from "../src/linq"
 
 describe("convertion to array tests", () => {
-    it("should conver the linqble to an array", () => {
+    it("should convert the linqble to an array", () => {
         const arr = linq([1,2,3,4,5]).toArray()
 
         assert(Array.isArray(arr))
+    })
+
+    it("should convert empty linqable to an empty array", () => {
+        const arr = linq([]).toArray()
+
+        assert(Array.isArray(arr))
+        assert.equal(arr.length, 0)
     })
 })
