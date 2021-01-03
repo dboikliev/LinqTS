4dfcfc8b035562a3ca6a2e72a9e7e03ae7bf4771
dboikliev <deyanpb@hotmail.com>
2021-01-03 23:02:07 +0200

    Added handling for sequences of different lengths.

--- src/linq/iterables/cartesian.ts
+++ src/linq/iterables/cartesian.ts
@@ -5,7 +5,7 @@ export class Cartesian<TFirst, TSecond> implements ElementsWrapper<TFirst | TSec
   private cacheFirst = new Map<number, Promise<IteratorResult<TFirst>> | IteratorResult<TFirst>>()
   private cacheSecond = new Map<number, Promise<IteratorResult<TSecond>> | IteratorResult<TSecond>>()
 
-  constructor(private readonly first: Iterable<TFirst> | AsyncIterable<TFirst>, 
+  constructor(private readonly first: Iterable<TFirst> | AsyncIterable<TFirst>,
     private readonly second: Iterable<TSecond> | AsyncIterable<TSecond>) {
   }
 
@@ -26,18 +26,16 @@ export class Cartesian<TFirst, TSecond> implements ElementsWrapper<TFirst | TSec
     let row = 0
     let currentRow = 0
     let currentCol = 0
+    let firstLen = 0
+    let secondLen = 0
     while (true) {
       let first
       if (this.cacheFirst.has(firstIndex)) {
         first = this.cacheFirst.get(firstIndex)
       } else {
         const result = itFirst.next()
-        this.cacheFirst.set(firstIndex, result)
-        if (result.done) {
-          currentRow = 0
-          return
-        }
         first = result
+        this.cacheFirst.set(firstIndex, result)
       }
 
       let second
@@ -45,28 +43,41 @@ export class Cartesian<TFirst, TSecond> implements ElementsWrapper<TFirst | TSec
         second = this.cacheSecond.get(secondIndex)
       } else {
         const result = itSecond.next()
-        if (!result.done) {
-          this.cacheSecond.set(secondIndex, result)
-          second = result
-        } else {
-          // currentCol = 0
-        }
+        second = result
+        this.cacheSecond.set(secondIndex, result)
       }
-
-      yield [first.value, second.value]
 
       if (currentRow === 0) {
         currentCol = 0
-        currentRow = row + 1
-        row++
+        currentRow = ++row
       } else {
         currentRow--
         currentCol++
       }
 
-
       firstIndex = currentRow
       secondIndex = currentCol
+
+
+      if (first.done && second.done) {
+        break
+      }
+
+      if (first.done) {
+        firstIndex = currentRow % firstLen
+        continue
+      } else {
+        firstLen++
+      }
+
+      if (second.done) {
+        secondIndex = currentCol % secondLen
+        continue
+      } else {
+        secondLen++
+      }
+
+      yield [first.value, second.value]
     }
   }
 
@@ -83,51 +94,59 @@ export class Cartesian<TFirst, TSecond> implements ElementsWrapper<TFirst | TSec
     let row = 0
     let currentRow = 0
     let currentCol = 0
+    let firstLen = 0
+    let secondLen = 0
     while (true) {
-      let first: Promise<IteratorResult<TFirst>>
+      let firstResult: Promise<IteratorResult<TFirst>>
       if (this.cacheFirst.has(firstIndex)) {
-        first = this.cacheFirst.get(firstIndex) as Promise<IteratorResult<TFirst>>
+        firstResult = this.cacheFirst.get(firstIndex) as Promise<IteratorResult<TFirst>>
       } else {
         const result = itFirst.next()
         this.cacheFirst.set(firstIndex, result)
-        first = result
+        firstResult = result
       }
 
-      let second: Promise<IteratorResult<TSecond>>
+      let secondResult: Promise<IteratorResult<TSecond>>
       if (this.cacheSecond.has(secondIndex)) {
-        second = this.cacheSecond.get(secondIndex) as Promise<IteratorResult<TSecond>>
+        secondResult = this.cacheSecond.get(secondIndex) as Promise<IteratorResult<TSecond>>
       } else {
         const result = itSecond.next()
         this.cacheSecond.set(secondIndex, result)
-        second = result
+        secondResult = result
       }
 
       if (currentRow === 0) {
         currentCol = 0
-        currentRow = row + 1
-        row++
+        currentRow = ++row
+      } else {
+        currentRow--
+        currentCol++
       }
-
-      currentRow--
-      currentCol++
-
-      const f = (await first)
-      const s = (await second)
-
-      if (f.done) {
-        currentRow = 0
-        continue
-      }
-
-      if (s.done) {
-        currentCol = 0
-        continue
-      }
-
-      yield [f.value, s.value]
 
       firstIndex = currentRow
       secondIndex = currentCol
+
+      const [first, second] = await Promise.all([firstResult, secondResult])
+
+      if (first.done && second.done) {
+        break
+      }
+
+      if (first.done) {
+        firstIndex = currentRow % firstLen
+        continue
+      } else {
+        firstLen++
+      }
+
+      if (second.done) {
+        secondIndex = currentCol % secondLen
+        continue
+      } else {
+        secondLen++
+      }
+
+      yield [first.value, second.value]
     }
   }
 
